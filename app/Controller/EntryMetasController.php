c0ff9fb54ce624ee94f2694a0972b0c498ac8021
Andy Basuki <andy_basuki_88@yahoo.com>
2015-10-30 14:22:57 +0700

    .

--- app/Controller/EntryMetasController.php
+++ app/Controller/EntryMetasController.php
@@ -3,6 +3,28 @@ class EntryMetasController extends AppController {
 	var $name = 'EntryMetas';
 	public function beforeFilter(){
         parent::beforeFilter();
+        $this->Auth->allow('cron');
+    }
+    
+    /*
+        Cron Jobs Function ( run daily @ 03.00 AM ) !!
+    */
+    public function cron($fakeauth)
+    {
+        if($fakeauth == 'test') // test cron jobs function !!
+		{
+            $sql = $this->Setting->findByKey('custom-pagination');
+			$this->Setting->id = $sql['Setting']['id'];
+			$this->Setting->saveField('value' , $sql['Setting']['value'] + 1);
+		}
+		else
+		{
+			throw new NotFoundException('Error 404 - Not Found');
+		}
+        
+        // end of cron jobs !!
+        dpr("Cron Jobs SUCCESS! (".date('Y-m-d H:i:s').")");
+        exit;
     }
 
     function deleteTempThumbnails()
